51f991e8487d7065a9a427682f8584e037651fdb
CGrant303 <chrisgrant303@gmail.com>
2018-10-12 14:44:51 -0500

    updated infoWindows to show dB content

--- client/src/components/MapSolo/MapSolo.js
+++ client/src/components/MapSolo/MapSolo.js
@@ -100,7 +100,7 @@ class MapSolo extends Component {
                     venues: response.data.courts, geolocationErr: false,
                     venuesAPIHit: true
                 })
-                this.initMap()
+                // this.initMap()
 
             })
             .catch(error => {
@@ -188,6 +188,7 @@ class MapSolo extends Component {
         this.state.fighters.map(function (fighters) {
 
             var contentString = fighters.name;
+            console.log("fighters.type");
 
             // Create A Marker
             var icon = {
@@ -219,7 +220,26 @@ class MapSolo extends Component {
         // // Display Dynamic Markers for Refs
         this.state.refs.map(function (refs) {
 
-            var contentString = refs.name;
+            var contentString = '<div id="content">' +
+                '<div id="siteNotice">' +
+                '</div>' +
+                '<h1 id="firstHeading" class="firstHeading">' + [refs.name] + '</h1>' +
+                '<div id="bodyContent">' +
+                '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
+                'sandstone rock formation in the southern part of the ' +
+                'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) ' +
+                'south west of the nearest large town, Alice Springs; 450&#160;km ' +
+                '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major ' +
+                'features of the Uluru - Kata Tjuta National Park. Uluru is ' +
+                'sacred to the Pitjantjatjara and Yankunytjatjara, the ' +
+                'Aboriginal people of the area. It has many springs, waterholes, ' +
+                'rock caves and ancient paintings. Uluru is listed as a World ' +
+                'Heritage Site.</p>' +
+                '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">' +
+                'https://en.wikipedia.org/w/index.php?title=Uluru</a> ' +
+                '(last visited June 22, 2009).</p>' +
+                '</div>' +
+                '</div>';
 
             var icon = {
                 url: refImage, // url
